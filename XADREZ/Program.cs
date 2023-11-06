3aee5c243a11d52586505d6e73a26e1c3ccd79ae
Ti7801 <tiagodaltro19@gmail.com>
2023-11-06 10:50:16 -0300

    Adiconando novos arquivos ao repositório do Sistema de Xadrez.

---
+++ XADREZ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using XADREZ.tabuleiro;
+
+namespace XADREZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CAMADA DE APLICAÇÃO  
+            // CAMADA JOGO DE XADREZ
+            // CAMADA TABULEIRO
+
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XADREZ/tabuleiro/Posicao.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace XADREZ.tabuleiro
+{
+    internal class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha= linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha
+                + ", "
+                + Coluna;
+        }
+    }
+}
