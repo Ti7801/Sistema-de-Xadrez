c24f2b4ea18ec927107b635cac1fab7347f848d2
Ti7801 <tiagodaltro19@gmail.com>
2023-11-07 19:34:17 -0300

    Adicionando validações para as peças Part 1.

---
+++ XADREZ/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace XADREZ.tabuleiro
+{
+    internal class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg) 
+        {
+        }
+    }
+}
